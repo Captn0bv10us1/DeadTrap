c00f42a7a983a5f059020351a3e6b5bcd53d127b
Captn0bv10us1 <99123481+Captn0bv10us1@users.noreply.github.com>
2022-02-06 14:39:41 -0800

    Update banner.py

--- Style/banner.py
+++ Style/banner.py
@@ -5,10 +5,10 @@ class colors:
     yellow =  '\033[1;33m'
 
 def banner():
-    path ='C:\Users\Administrator\Documents\GitHub\DeadTrap\Style/logos'
+    path ='C:\Users\Administrator\Documents\GitHub\DeadTrap\Style'
     files = os.listdir(path)
     index = random.randrange(0, len(files))
-    f = open(f'C:\Users\Administrator\Documents\GitHub\DeadTrap\Style/{files[index]}' , 'r')
+    f = open(f'C:\Users\Administrator\Documents\GitHub\DeadTrap\Style{files[index]}' , 'r')
     contents = f.read()
     print(colors.yellow + contents)
     f.close()
